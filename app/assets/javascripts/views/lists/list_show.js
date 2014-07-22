493131f55d7e608e20551e67c119d256bba968de
drwrchrds <drwrchrds@gmail.com>
2014-07-21 17:58:39 -0700

    style add-card form and list-cards

--- app/assets/javascripts/views/lists/list_show.js
+++ app/assets/javascripts/views/lists/list_show.js
@@ -1,10 +1,12 @@
 TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
   template: JST['lists/show'],
-  className: "list",
-
+  
+  className: "list-display",
+  
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
     this.listenTo(this.model.cards(), 'add', this.addCard);
+    this.listenTo(this.model.cards(), 'add resize', this.setHeight);
   },
 
   addCard: function (card) {
@@ -19,14 +21,37 @@ TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
       list: this.model
     });
     this.$el.html(content);
-
+    this.$el.data('list-id', this.model.id);
+    
     this.renderCards();
     this.renderFooter();
+    setTimeout(this.setHeight.bind(this));
+    // this.setHeight();
     return this;
   },
 
   renderCards: function () {
-    this.model.cards().each(this.addCard.bind(this));
+    this.model.cards().each(this.addCard.bind(this));TrelloClone.Views.BoardsIndex = Backbone.View.extend({
+  template: JST['boards/index'],
+  
+  className: 'boards-index',
+
+  initialize: function () {
+    this.listenTo(this.collection, 'sync', this.render);
+    $('body').css('background-color', 'rgb(255, 255, 255)')
+    
+  },
+  
+  render: function () {
+    var content = this.template({
+      boards: this.collection
+    });
+
+    this.$el.html(content);
+    return this;
+  }
+});
+
   },
 
   renderFooter: function () {
@@ -34,5 +59,21 @@ TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
       collection: this.model.cards()
     });
     this.addSubview(".list-footer", formView);
+  },
+  
+  setHeight: function() {
+    this.$('.list-cards').css('')
+    var listsHeight = this.$el.parent().height();
+    var listHeight = this.$el.height();
+    var headerHeight = this.$('.list-heading').height();
+    var footerHeight = this.$('.list-footer').height();
+    var cardsHeight = this.$('.list-cards').height();
+    
+    // debugger
+    
+    // if (cardsHeight + headerHeight + footerHeight > listHeight) {
+    this.$('.list-cards').css('max-height', 
+          listsHeight - headerHeight - footerHeight - 11);
+    // }
   }
 });
